1fc2b8d9a7878d7d285a27c6f01c42fc47d5dd44
Lisa Jensen <leese_38@yahoo.com>
2019-06-07 13:03:23 -0700

    add variable sum and change functions to use less than or equal too to simplify obtaining survey results

--- js/scripts.js
+++ js/scripts.js
@@ -5,26 +5,27 @@ $(document).ready(function() {
      var question3 = parseInt($("#question3 option:selected").val());
      var question4 = parseInt($("#question4 option:selected").val());
      var question5 = parseInt($("#question5 option:selected").val());
-     
+
      if (!question1 || !question2 || !question3 || !question4 || !question5) {
        alert("Please select an answer for all questions.")
      } else {
 
-       if (question1 === "1" && question2 ==="1" && question3 ==="1" && question4 ==="1" && question5 ==="1") {
+       var sum = (question1 + question2 + question3 + question4 + question5)
+
+       if (sum <= 8) { console.log("here");
          $("#javascript").show();
          $("#cSharp").hide();
          $("#react").hide();
-       } else if (question1 === "2" && question2 ==="2" && question3 ==="2" && question4 ==="2" && question5 ==="2") {
+       } else if (sum <= 12) {
          $("#javascript").hide();
          $("#cSharp").show();
          $("#react").hide();
-       } else if (question1 === "3" && question2 ==="3" && question3 ==="3" && question4 ==="3" && question5 ==="3") {
+       } else {
          $("#javascript").hide();
-         $("#cSharp").show();
-         $("#react").hide();
+         $("#cSharp").hide();
+         $("#react").show();
        }
      }
-
 
 
 
