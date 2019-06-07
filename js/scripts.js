9990ac32b3db39c58c43a6fe7434c370516d70ba
Lisa Jensen <leese_38@yahoo.com>
2019-06-07 10:41:45 -0700

    add alert if not all questions are answered

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,20 @@
 $(document).ready(function() {
  $("#coderSurvey").submit(function(event) {
-     var question1 = ($("#question1 option:selected").val());
-     var question2 = ($("#question2 option:selected").val());
-     var question3 = ($("#question3 option:selected").val());
-     var question4 = ($("#question4 option:selected").val());
-     var question5 = ($("#question5 option:selected").val());
+     var question1 = parseInt($("#question1 option:selected").val());
+     var question2 = parseInt($("#question2 option:selected").val());
+     var question3 = parseInt($("#question3 option:selected").val());
+     var question4 = parseInt($("#question4 option:selected").val());
+     var question5 = parseInt($("#question5 option:selected").val());
 
      if (!question1 || !question2 || !question3 || !question4 || !question5) {
        alert("Please select an answer for all questions.")
      } else {
 
-       if ()
+       if (question1 === "1" && question2 ==="1" && question3 ==="1" && question4 ==="1" && question5 ==="1") {
+         $("#javascript").show();
+         $("#cSharp").hide();
+         $("#react").hide();
+       }
      }
 
 
