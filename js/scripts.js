c093d0584d0431f8046a35191e674e804361ea16
Lisa Jensen <leese_38@yahoo.com>
2019-06-07 10:11:51 -0700

    add form with 5 questions and 3 options for each, assigned variables for inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,23 @@
 $(document).ready(function() {
  $("#coderSurvey").submit(function(event) {
+     var question1 = ($("#question1 option:selected").val());
+     var question2 = ($("#question2 option:selected").val());
+     var question3 = ($("#question3 option:selected").val());
+     var question4 = ($("#question4 option:selected").val());
+     var question5 = ($("#question5 option:selected").val());
+
+     if (!question1 || !question2 || !question3 || !question4 || !question5) {
+       alert("Please select an answer for all questions.")
+     } else {
+
+       if ()
+     }
+
+
+
+
+
+
    event.preventDefault();
-
-
-
-
-
-
+ });
 });
